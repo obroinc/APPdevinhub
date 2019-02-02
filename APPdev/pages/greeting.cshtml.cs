fa17b4536c9f7913c76a39f2a196dc807ebd2fd0
HPLaptop <HPLaptop@CLEMENTLAPTOP>
2019-02-02 16:12:43 +0000

    updating

---
+++ APPdev/pages/greeting.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace APPdev.pages
+{
+    public class greetingModel : PageModel
+    {
+        public string message { get; set; } = "Hello there this is  amessage from greetin.cshtml.cs";
+        public void OnGet()
+        {
+            if (DateTime.Now.Hour >= 12)
+            {
+                message = "Goodafternoon";
+            }
+            else{
+                message = "Good Morning";
+            }
+
+        }
+    }
+}
---
+++ APPdev/pages/ws1.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace APPdev.pages
+{
+    public class ws1Model : PageModel
+
+    {
+        public string message { get; set; }
+
+        public void OnGet()
+        {
+
+        }
+        public void OnPost(string name)
+        {
+            message = string.Format("Hello there {0}", name);
+        }
+    }
+}
